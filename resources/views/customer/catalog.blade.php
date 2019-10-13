8e6e2df16d93911efaab994b904901ebf981d429
Rodrigo Soares <rodpluto@outlook.com.br>
2019-10-13 14:30:25 -0300

    ajustes para mobile

--- resources/views/customer/catalog.blade.php
+++ resources/views/customer/catalog.blade.php
@@ -4,21 +4,14 @@
 
     <div class="row">
         @foreach( $products as $product )
-        <div class="col-xs-4 col-sm-4 col-md-2">
-            <div class="box box-solid">
-                <div class="box-body text-center">
-                    <div class="thumbnail text-center" onclick="modalAddItem({{ $product->id }})">
-                        <img src="{{ $product->image }}" alt="...">
-                    </div>
+            <div class="col-xs-4 col-sm-4 col-md-2 text-center">
+                <div class="thumbnail" onclick="modalAddItem({{ $product->id }})">
+                    <img src="{{ $product->image }}" alt="...">
                     <div class="prod-box-info">
-                        <h5>
-                            <i>{{ $product->ean }}</i>
-                        </h5>
                         <h5>{{ $product->name }}</h5>
                     </div>
                 </div>
             </div>
-        </div>
         @endforeach
     </div>
 
@@ -26,5 +19,5 @@
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?{{ date('YmdHis') }}">
 @stop
