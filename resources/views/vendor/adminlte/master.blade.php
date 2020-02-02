11361ce8b031368b63beca005d50ce304aa79571
Rodrigo Soares Souza <rodsoadev@gmail.com>
2020-02-02 16:11:00 -0300

    Adicionando favicon temporario

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -27,6 +27,9 @@
 
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('48png.png') }}" type="image/png">
 </head>
 <body class="hold-transition @yield('body_class')">
 
